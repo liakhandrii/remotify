6a464e9a5b5d0216598bcf5ac6d7489f29b5ab68
Dima <dima@MacBook-Pro-Dima.local>
2015-04-25 20:53:30 +0300

    vol.1.2

--- Remotify_Server/src/RemotifyMain.java
+++ Remotify_Server/src/RemotifyMain.java
@@ -9,39 +9,40 @@ import java.net.SocketException;
 import java.util.Enumeration;
 
 public class RemotifyMain {
-
-	public static void main(String[] args) throws IOException {
-		getMyIP();
-		ServerOperator so = new ServerOperator();
-		Socket socket = null;
-		try {
-			socket = new Socket("remotify.cloudapp.net", 10481);
-			final PrintWriter out = new PrintWriter(new BufferedWriter(
-					new OutputStreamWriter(socket.getOutputStream())), true);
-			out.println("server_connect");
-			Thread connectionAlive = new Thread() {
-				@Override
-				public void run() {
-					while (true) {
-						out.println("c");
-						try {
-							Thread.sleep(2000);
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					}
-
-				}
-			};
-			connectionAlive.setDaemon(true);
-			connectionAlive.start();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		so.startServer();
-
+    
+    public static void main(String[] args) throws IOException {
+        getMyIP();
+        ServerOperator so = new ServerOperator();
+        Socket socket = null;
+        try {
+            socket = new Socket("remotify.cloudapp.net", 10481);
+            final PrintWriter out = new PrintWriter(new BufferedWriter(
+                                                                       new OutputStreamWriter(socket.getOutputStream())), true);
+            out.println("server_connect");
+            out.println(getMyIP());
+            Thread connectionAlive = new Thread() {
+                @Override
+                public void run() {
+                    while (true) {
+                        out.println("c");
+                        try {
+                            Thread.sleep(2000);
+                        } catch (InterruptedException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                    }
+                    
+                }
+            };
+            connectionAlive.setDaemon(true);
+            connectionAlive.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        so.startServer();
+        
         if (socket != null) {
             PrintWriter out = new PrintWriter(new BufferedWriter(
                                                                  new OutputStreamWriter(socket.getOutputStream())), true);
@@ -71,4 +72,4 @@ public class RemotifyMain {
         return "Bad IP";
     }
     
-}
+
