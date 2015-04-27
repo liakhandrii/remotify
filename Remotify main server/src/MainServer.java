a7871b778e8678decb82d14be233b7bd270cd3aa
Volodymyr <volodymyr@localhost.localdomain>
2015-04-27 03:21:57 +0300

    work with server list

--- Remotify main server/src/MainServer.java
+++ Remotify main server/src/MainServer.java
@@ -16,26 +16,84 @@ import java.util.HashSet;
 
 public class MainServer implements Runnable {
 
+	private final ServerSocket servSocket;
+	private static final int PORT = 10481;
+	private final Connection con;
+
+	public MainServer(Connection con) throws IOException {
+		servSocket = new ServerSocket(PORT);
+		this.con = con;
+	}
+
 	@Override
 	public void run() {
 		
+
 }
 
 class ServerThread implements Runnable {
 
+	private Socket socket;
+	private final Connection con;
+
+	public ServerThread(Socket socket, Connection con) throws IOException {
+		this.socket = socket;
+		socket.setSoTimeout(10000);
+		this.con = con;
 	}
 
-			
-	private void addServerToList(String extIP, String intIP) {
+	@Override
+	public void run() {
 		
+
+			
+	}
+
+	private void addServerToList(String extIP, String intIP) {
+		Statement st;
+		try {
+			st = con.createStatement();
+			st.executeUpdate("INSERT INTO servers (extIP, intIP) VALUES ('"
+					+ extIP + "', '" + intIP + "');");
+			System.out.println("Added a server " + extIP + " " + intIP);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	private ArrayList<String> listServers(String clientExtIP) {
-		
+		ArrayList<String> res = new ArrayList<String>();
+
+		Statement st;
+		try {
+			st = con.createStatement();
+			ResultSet rs = st
+					.executeQuery("SELECT * FROM servers WHERE extIP='"
+							+ clientExtIP + "';");
+			while (rs.next()) {
+				res.add(rs.getString("intIP"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		HashSet<String> hs = new HashSet<>();
+		hs.addAll(res);
+		res.clear();
+		res.addAll(hs);
+		return res;
 	}
 
 	private void removeServerFromList(String extIP, String intIP) {
-		
+		Statement st;
+		try {
+			st = con.createStatement();
+			st.executeUpdate("DELETE FROM servers WHERE extIP='" + extIP
+					+ "' AND intIP='" + intIP + "';");
+			System.out.println("Removed a server " + extIP + " " + intIP);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
