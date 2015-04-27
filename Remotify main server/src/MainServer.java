3f05391cf2fafd889afb62473dd91750728ad011
Volodymyr <volodymyr@localhost.localdomain>
2015-04-27 03:10:12 +0300

    initiale commit

---
+++ Remotify main server/src/MainServer.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class MainServer implements Runnable {
+
+	@Override
+	public void run() {
+		
+}
+
+class ServerThread implements Runnable {
+
+	}
+
+			
+	private void addServerToList(String extIP, String intIP) {
+		
+	}
+
+	private ArrayList<String> listServers(String clientExtIP) {
+		
+	}
+
+	private void removeServerFromList(String extIP, String intIP) {
+		
+	}
+
+}
